0c2f8a76250b2458af45c01e883172299b304807
wey <149201216@qq.com>
2020-01-04 21:09:49 +0800

    头部组件的顶部条

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,9 +5,14 @@
     <nav-footer></nav-footer>
   </div>
 </template>
-
 <script>
+import NavHeader from "./../components/NavHeader";
+import NavFooter from "./../components/NavFooter";
 export default {
-  
-}
+  name: "nav-home",
+  components: {
+    NavHeader,
+    NavFooter
+  }
+};
 </script>
