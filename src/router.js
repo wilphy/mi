2b37e8de5aa5ff8660a01c4c3dce6b91013c4048
wey <149201216@qq.com>
2020-01-04 21:02:54 +0800

    router

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './pages/home'
+import Index from './pages/index'
+
+Vue.use(Router);
+
+export default new Router({
+  routes:[
+    {
+      path:'/',
+      name:'home',
+      component:Home,
+      redirect:'/index',
+      children:[
+        {
+          path: '/index',
+          name: 'index',
+          component: Index,
+        }
+      ]
+    }
+  ]
+});
