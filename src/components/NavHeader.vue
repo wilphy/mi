6ea3ee18521ef437dc554ca9a0a21b90433f6481
wey <149201216@qq.com>
2020-01-05 11:51:23 +0800

    头部的商品导航栏

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -12,8 +12,8 @@
         <div class="topbar-user">
           <a href="javascript:;">登录</a>
           <a href="javascript:;">注册</a>
-          <a href="javascript:;" class="my-cart"
-            ><span class="icon-cart"></span>购物车</a
+          <a href="javascript:;" class="my-cart">
+            <span class="icon-cart"></span>购物车</a
           >
         </div>
       </div>
@@ -29,12 +29,31 @@
         <div class="header-menu">
           <div class="item-menu">
             <span>小米手机</span>
+            <div class="children">
+              <ul>
+                <li
+                  class="product"
+                  v-for="(item, index) in phoneList"
+                  :key="index"
+                >
+                  <a href="" target="_blank">
+                    <div class="pd-img">
+                      <img :src="item.mainImage" :alt="item.subtitle" />
+                    </div>
+                    <div class="pd-name">{{ item.name }}</div>
+                    <div class="pd-price">{{ item.price | currency }}</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="item-menu">
             <span>RedMi手机</span>
+            <div class="children"></div>
           </div>
           <div class="item-menu">
             <span>电视</span>
+            <div class="children"></div>
           </div>
         </div>
 
@@ -51,11 +70,40 @@
 
 <script>
 export default {
-  name: "nav-header"
+  name: "nav-header",
+  data() {
+    return {
+      phoneList: []
+    };
+  },
+  filters: {
+    currency(val) {
+      if (!val) return "0.00";
+      return "￥" + val.toFixed(2) + "元";
+    }
+  },
+  mounted() {
+    this.getProductList();
+  },
+  methods: {
+    getProductList() {
+      this.axios
+        .get("/products", {
+          params: {
+            categoryId: "100012",
+            pageSize: 6
+          }
+        })
+        .then(res => {
+          this.phoneList = res.list;
+        });
+    }
+  }
 };
 </script>
 
 <style lang="scss">
+@import "../assets/scss/config.scss";
 @import "../assets/scss/base.scss";
 @import "../assets/scss/mixin.scss";
 
@@ -87,25 +135,26 @@ export default {
 
   .nav-header {
     .container {
+      position: relative;
       height: 112px;
       @include flex();
       .header-logo {
         display: inline-block;
         width: 55px;
         height: 55px;
-        background-color: #ff6600;
+        background-color: $colorA;
         a {
           display: inline-block;
           width: 110px;
           height: 55px;
           &:before {
             content: "";
-            @include bgImg(55px, 55px, "/imgs/mi-logo.png",55px);
+            @include bgImg(55px, 55px, "/imgs/mi-logo.png", 55px);
             transition: margin 0.3s;
           }
           &:after {
             content: "";
-            @include bgImg(55px, 55px, "/imgs/mi-home.png",55px);
+            @include bgImg(55px, 55px, "/imgs/mi-home.png", 55px);
           }
           &:hover:before {
             margin-left: -55px;
@@ -127,6 +176,65 @@ export default {
             cursor: pointer;
           }
           &:hover {
+            color: $colorA;
+            .children {
+              height: 220px;
+              opacity: 1;
+            }
+          }
+          .children {
+            position: absolute;
+            top: 112px;
+            left: 0;
+            width: 1226px;
+            height: 0;
+            opacity: 0;
+            overflow: hidden;
+            border-top: 1px solid #e5e5e5;
+            box-shadow: 0 7px 6px 0px rgba(0, 0, 0, 0.1);
+            z-index: 10;
+            transition: all 0.5s;
+            .product {
+              position: relative;
+              float: left;
+              width: 16.6%;
+              height: 220px;
+              font-size: 12px;
+              line-height: 12px;
+              text-align: center;
+              &:before {
+                content: "";
+                position: absolute;
+                top: 28px;
+                right: 0;
+                border-left: 1px solid $colorF;
+                height: 100px;
+                width: 1px;
+              }
+              &:last-child:before {
+                display: none;
+              }
+              a {
+                display: inline-block;
+              }
+              img {
+                width: auto;
+                height: 111px;
+                margin-top: 26px;
+              }
+              .pd-img {
+                height: 137px;
+              }
+              .pd-name {
+                font-weight: bold;
+                margin-top: 19px;
+                margin-bottom: 8px;
+                color: $colorB;
+              }
+              .pd-price {
+                color: $colorA;
+              }
+            }
           }
         }
       }
