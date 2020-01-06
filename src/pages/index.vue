cef403a1a254464abc46351aec172cc3a192e2bf
wey <149201216@qq.com>
2020-01-06 17:57:20 +0800

    首页的轮播图实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,99 @@
 <template>
-  <div>
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <swiper :options="swiperOption">
+          <!-- slides -->
+          <swiper-slide v-for="(item, index) in slideList" :key="index">
+            <a :href="'/#/product/' + item.id"><img :src="item.img" alt=""/></a>
+          </swiper-slide>
+          <!-- Optional controls -->
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+          <div class="swiper-scrollbar" slot="scrollbar"></div>
+        </swiper>
+      </div>
+      <div class="ads-box"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
+    </div>
     <service-bar></service-bar>
   </div>
 </template>
 
 <script>
-import ServiceBar from "../components/ServiceBar"
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
+import ServiceBar from "../components/ServiceBar";
 export default {
   name: "index",
   components: {
+    swiper,
+    swiperSlide,
     ServiceBar
+  },
+  data() {
+    return {
+      //轮播图配置
+      swiperOption: {
+        autoplay: true, //自动播放
+        loop: true, //无线循环
+        effect: "cube", //切换效果
+        cubeEffect: {
+          slideShadows: true,
+          shadow: true,
+          shadowOffset: 100,
+          shadowScale: 0.6
+        },
+        //分页器
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        //控制前进后退
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      },
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg"
+        },
+        {
+          id: "42",
+          img: "/imgs/slider/slide-2.jpg"
+        },
+        {
+          id: "42",
+          img: "/imgs/slider/slide-3.jpg"
+        },
+        {
+          id: "42",
+          img: "/imgs/slider/slide-4.jpg"
+        },
+        {
+          id: "42",
+          img: "/imgs/slider/slide-5.jpg"
+        }
+      ]
+    };
   }
 };
 </script>
+
+<style lang="scss">
+.index {
+  .swiper-box {
+    .swiper-container {
+      height: 451px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
+</style>
