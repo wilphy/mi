995af8f00022fe9962b473b45215893c33471971
wey <149201216@qq.com>
2020-01-16 21:04:34 +0800

    视频模块

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -6,6 +6,29 @@
       </template>
     </product-param>
 
+    <div class="content">
+      <div class="item-video">
+        <h2>60帧超慢动作摄影<br />慢慢回味每一瞬间的精彩</h2>
+        <p>
+          后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！<br />更能AI
+          精准分析视频内容，15个场景智能匹配背景音效。
+        </p>
+        <div class="video-bg"></div>
+        <div class="video-box">
+          <div class="overlay"></div>
+          <div class="video">
+            <span class="icon-close"></span>
+            <video
+              src="/imgs/product/video.mp4"
+              muted
+              autoplay
+              controls="controls"
+            ></video>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="h">123123123</div>
     <div class="h">123123123</div>
     <div class="h">123123123</div>
@@ -28,6 +51,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/scss/mixin.scss";
 .product {
   .h {
     height: 200px;
@@ -35,6 +59,34 @@ export default {
   }
   button {
     margin-left: 10px;
+  }
+  .video-box {
+    .overlay {
+      @include position(fixed);
+      background-color: #333;
+      opacity: 0.4;
+    }
+    .video {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 10;
+      .icon-close {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        cursor: pointer;
+        z-index: 11;
+        @include bgImg(20px, 20px, "/imgs/icon-close.png");
+      }
+      video {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        outline: none;
+      }
+    }
   }
 }
 </style>
