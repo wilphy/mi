72b1078166f2b49e8c4ad063d56322b23585c051
wey <149201216@qq.com>
2020-02-06 20:43:50 +0800

    修复顶部参数条与商品页显示不一致的问题

--- src/components/ProductParam.vue
+++ src/components/ProductParam.vue
@@ -2,7 +2,7 @@
   <div class="nav-bar" :class="{ is_fixed: isFixed }">
     <div class="container">
       <div class="pro-title">
-        小米8
+        {{ title }}
       </div>
       <div class="pro-param">
         <a href="javascript:;">概述</a><span>|</span>
@@ -16,6 +16,9 @@
 <script>
 export default {
   name: "nav-bar",
+  props: {
+    title: String
+  },
   data() {
     return {
       isFixed: false
@@ -46,6 +49,7 @@ export default {
   line-height: 70px;
   border-top: 1px solid $colorH;
   background-color: $colorG;
+  z-index: 10;
   &.is_fixed {
     position: fixed;
     top: 0;
