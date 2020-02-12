0abc8d91df2e9bfabd9f9caaff24facc294fd9c6
wey <149201216@qq.com>
2020-02-12 20:58:39 +0800

    OrderHeader组件

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <order-header></order-header>
+    <order-header title="我的购物车">
+      <template v-slot:tip>
+        <span>温馨提示：产品是否购买成功，以最终订单页面为准</span>
+      </template>
+    </order-header>
     购物车
     <nav-footer></nav-footer>
   </div>
 </template>
 <script>
 import NavFooter from "./../components/NavFooter";
+import OrderHeader from "./../components/OrderHeader";
 export default {
   name: "index",
   components: {
-    NavFooter
+    NavFooter,
+    OrderHeader
   }
 };
 </script>
