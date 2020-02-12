b8c80b5498618fabe2298fe4e76bd8b45ac25086
wey <149201216@qq.com>
2020-02-12 20:57:36 +0800

    提取公共的header-logo样式

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -215,30 +215,6 @@ export default {
       position: relative;
       height: 112px;
       @include flex();
-      .header-logo {
-        display: inline-block;
-        width: 55px;
-        height: 55px;
-        background-color: $colorA;
-        a {
-          display: inline-block;
-          width: 110px;
-          height: 55px;
-          &:before {
-            content: "";
-            @include bgImg(55px, 55px, "/imgs/mi-logo.png", 55px);
-            transition: margin 0.3s;
-          }
-          &:after {
-            content: "";
-            @include bgImg(55px, 55px, "/imgs/mi-home.png", 55px);
-          }
-          &:hover:before {
-            margin-left: -55px;
-            transition: margin 0.3s;
-          }
-        }
-      }
       .header-menu {
         display: inline-block;
         width: 600px;
